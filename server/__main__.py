8cc8c64eb9b093260763797b7467b191e80d94c9
Mateus Abelli <mateusabelli@gmail.com>
2023-04-21 15:07:11 -0300

    Implement the CLI

--- server/__main__.py
+++ server/__main__.py
@@ -1,8 +1,69 @@
-from .server import json_server
+from .server import server
+from argparse import ArgumentParser
+import logging
 
 def main():
-    json_server.start_io()
+    parser = ArgumentParser(description="HTTP Language Server")
+    server_methods = parser.add_mutually_exclusive_group()
+
+    # Server methods
+    server_methods.add_argument(
+        "--stdio", help="start the server using stdio", action="store_true"
+    )
+    server_methods.add_argument(
+        "--tcp", help="start the server using tcp", action="store_true"
+    )
+
+    # General arguments
+    parser.add_argument("--silent", help="hide start log messages", action="store_true")
+    parser.add_argument(
+        "--debug", help="write debug logs to httpls.log file", action="store_true"
+    )
+    parser.add_argument(
+        "--host",
+        help="host <ip> address for tcp connection, default: 127.0.0.1",
+        type=str,
+        metavar="<ip>",
+        default="127.0.0.1",
+    )
+    parser.add_argument(
+        "--port",
+        help="port <number> for tcp connection, default: 2087",
+        type=int,
+        metavar="<number>",
+        default=2087,
+    )
+
+    args = parser.parse_args()
+
+    if args.stdio:
+        if not args.silent:
+            print("HTTP Language Server now running in STDIO mode...")
+            print("Press CTRL + C to stop.")
+
+        if args.debug:
+            logging.basicConfig(
+                filename="httpls.log", level=logging.DEBUG, filemode="w"
+            )
+
+        server.start_io()
+
+    elif args.tcp:
+        if not args.silent:
+            print("Sorry, this feature has not been implemented yet.")
+
+        if args.debug:
+            # logging.basicConfig(filename="httpls.log", level=logging.DEBUG, filemode="w")
+            print(
+                f"HTTP Language Server now running in TCP mode at {args.host}:{args.port}"
+            )
+            print("Press CTRL + C to stop.")
+
+        # TODO: Implement the TCP server connection
+        # server.start_tcp(args.host, args.port)
+    else:
+        parser.print_help()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
